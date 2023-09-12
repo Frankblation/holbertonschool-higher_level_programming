918a521a35cc206f5b743dc68a82eeaa15149766
Frankblation <frankblation@gmail.com>
2023-09-12 02:33:52 -0700

    6

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-str1 += ' ' + str2
-print("Welcome to {}!".format(str1))
+str1 = "object"
+str2 = "oriented"
+str3 = "programming"
+str4 = "with"
+str5 = "Python"
+print(str1[0:6] + str2[0:9] + str3[0:11] + str4[0:4] + str5)
