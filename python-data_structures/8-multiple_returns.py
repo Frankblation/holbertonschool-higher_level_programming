b949958159e72276f404f6df38906585704fa55a
Frankblation <frankblation@gmail.com>
2023-09-11 20:02:01 -0700

    max

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    # Check if the sentence is empty
+
     if len(sentence) == 0:
         first_char = None
     else:
         first_char = sentence[0]
-
-    # Calculate the length of the string
     length = len(sentence)
-
-    # Return a tuple with length and first character
     return (length, first_char)
---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if my_list is None:
+        return None
+    if len(my_list) == 0:
+        return None
+    highest = my_list[0]
+    for num in my_list:
+        if num > highest:
+            highest = num
+    print("Max: {:d}".format(highest))
+    return highest
