5aafed728b1e0041f0c076dd4a1b97873d5b012b
Frankblation <frankblation@gmail.com>
2023-09-11 20:52:27 -0700

    does it need to be a float?

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-
 def max_integer(my_list=[]):
     if my_list is None:
         return None
     if len(my_list) == 0:
         return None
-    highest = my_list[0]
+
+    highest = float("-inf")
+
     for num in my_list:
         if num > highest:
             highest = num
+
     print("Max: {:d}".format(highest))
     return highest
