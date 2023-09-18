ddbe4479c246d329b10595dfd5381b008d4d2448
Frankblation <frankblation@gmail.com>
2023-09-17 17:47:33 -0700

    indention matters

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""This class defines Square"""
-
 
 class Square:
-    """this is a defining size and self as instance"""
     def __init__(self, size=0):
+        self.__size = size
 
-        @property
-        def size(self):
-            return self.__size
+    @property
+    def size(self):
+        return self.__size
 
-        def size(self, value):
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = value
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
         return self.__size**2
