acee9f3d28187a03645242a04ab9049a8b7aafb9
Frankblation <frankblation@gmail.com>
2023-09-18 13:55:57 -0700

    1

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module 0.add_integer
+Method used to add two integers
+"""
+
+
+def add_integer(a, b=98):
+    """Returns int versions of a + b"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import unittest
+
+def max_integer(list=[]):
