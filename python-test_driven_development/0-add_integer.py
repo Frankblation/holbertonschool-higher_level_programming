d712f6c66a43178707b03b10313b004f2ce8eef3
Frankblation <frankblation@gmail.com>
2023-09-18 14:50:19 -0700

    value error

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,11 +4,16 @@ Module 0.add_integer
 Method used to add two integers
 """
 
-
 def add_integer(a, b=98):
     """Returns int versions of a + b"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+
+    result = int(a) + int(b)
+
+    if result != a + b:
+        raise ValueError("Result is not an integer")
+
+    return result
