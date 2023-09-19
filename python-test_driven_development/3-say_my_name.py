9cc7891e5a103747a5f52e9be5bda79e58197236
Frankblation <frankblation@gmail.com>
2023-09-19 16:21:14 -0700

    yessss

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -11,10 +11,11 @@ def say_my_name(first_name, last_name=""):
         first_name (st): first name
         last_name (str, optional): last name. Defaults to "".
     """
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
+    if not isinstance(first_name, str):
         raise
-    TypeError("first_name must be a string or last_name must be a string")
-
+    TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
     full_name = f"My name is {first_name} {last_name}" if last_name\
         else f"My name is {first_name}"
     print(full_name)
