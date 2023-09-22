953ccba7aec8207e8946b25c34bc0db2cdda9b5a
Frankblation <frankblation@gmail.com>
2023-09-21 22:12:47 -0700

    yes

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -5,4 +5,21 @@
 
 class Rectangle:
     """pass is an empty place holder for def"""
-    pass
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise
