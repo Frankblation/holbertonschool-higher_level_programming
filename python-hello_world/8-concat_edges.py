b9db7e0ba4b15dcd890913e59950c49b30b7e053
Frankblation <frankblation@gmail.com>
2023-09-22 08:58:12 -0700

    string

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[65:100]+ '\n')
+str = str[65:100]+ '\n'
+print(str)
