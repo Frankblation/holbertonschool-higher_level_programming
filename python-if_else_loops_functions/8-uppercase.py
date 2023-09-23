cf2a8b6a680a6cb9edb300dea27cc9eb849c67b0
Frankblation <frankblation@gmail.com>
2023-09-22 21:14:50 -0700

    [A

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 
 def uppercase(s):
-    result = ""
 
     for char in s:
-        if 97 >= ord(char) <= 122:
-            uppercase_char = chr(ord(char) - 32)
-        else:
-            uppercase_char = char
-
-        result += uppercase_char
-
-        print("{}\n".format(result))
+        if 97 <= ord(char) <= 122:
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
     print("")
