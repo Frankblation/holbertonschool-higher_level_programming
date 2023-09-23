0a8f8118a71500dd00f0249a78d392efcfd669bc
Frankblation <frankblation@gmail.com>
2023-09-22 21:44:03 -0700

    sys import

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]  # Exclude the script name
+
+    num_args = len(argv)
+
+    if num_args == 0:
+        print("0 arguments.")
+        print(".")
+    else:
+        print("{} argument{}:".format(num_args, 's' if num_args > 1 else ''))
+        for i, arg in enumerate(argv, start=1):
+            print("{}: {}".format(i, arg))
