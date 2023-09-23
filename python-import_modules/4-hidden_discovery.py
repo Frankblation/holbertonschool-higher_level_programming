a3d83899e3c63bd9c57e9361b9e2ed1583f18706
Frankblation <frankblation@gmail.com>
2023-09-22 21:55:10 -0700

    [A[A

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,30 +1,7 @@
 #!/usr/bin/python3
-import os
-import py_compile
-import sys
-
-def get_names_from_pyc(pyc_file):
-    if not os.path.isfile(pyc_file):
-        print(f"Error: '{pyc_file}' does not exist.")
-        return []
-
-    py_file = os.path.splitext(pyc_file)[0] + '.py'
-    if not os.path.isfile(py_file):
-        py_compile.compile(pyc_file, cfile=py_file)
-
-    module_name = os.path.splitext(os.path.basename(py_file))[0]
-    try:
-        module = __import__(module_name)
-    except ImportError:
-        print(f"Error: Failed to import module '{module_name}'")
-        return []
-
-    names = [name for name in dir(module) if not name.startswith("__")]
-    return sorted(names)
-
 if __name__ == "__main__":
-    pyc_file = "hidden_4.pyc"
-    names = get_names_from_pyc(pyc_file)
-
-    for name in names:
-        print(name)
+    import hidden_4
+    for i in dir(hidden_4):
+        if i[:2] != '__':
+            print("{:s}".format(i))
+        print(i)
