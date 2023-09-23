1980362d3369e4402211a492287e9a0c63a74e89
Frankblation <frankblation@gmail.com>
2023-09-22 20:55:25 -0700

    upper

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 def islower(c):
-    # Check if the character is a lowercase letter
     return 97 <= ord(c) <= 122
---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def uppercase(s):
+    result = ""
+
+    for char in s:
+        if 97 <= ord(char) <= 122:
+            uppercase_char = chr(ord(char) - 32)
+        else:
+            uppercase_char = char
+
+        result += uppercase_char
+
+    print(result + "\n")
