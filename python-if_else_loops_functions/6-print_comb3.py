da9cb26ae5e75545d32d250e8afef7550af5f911
Frankblation <frankblation@gmail.com>
2023-09-22 18:01:16 -0700

    [B

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -9,3 +9,4 @@ for tens_digit in range(10):
                 print("{:02d}".format(tens_digit * 10 + ones_digit), end='')
             if tens_digit < 8 or ones_digit < 9:
                 print(", ", end='')
+print()
