a704df9005dc13de683fb08d0d02c76baad33335
Frankblation <frankblation@gmail.com>
2023-09-22 20:46:11 -0700

    ?

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
-for tens_digit in range(0, 10):
-    for ones_digit in range(tens_digit + 1, 10):
-        if tens_digit != ones_digit:
-            if tens_digit == 0 and ones_digit == 1:
-                print("01", end='')
-            else:
-                print("{:02d}".format(tens_digit * 10 + ones_digit), end='\n')
-            if tens_digit < 8 or ones_digit < 9:
-                print(", ", end='')
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
