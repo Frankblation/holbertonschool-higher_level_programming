d6cdac92d191e9d827e9b49b6d03bd58d6636aaf
Frankblation <frankblation@gmail.com>
2023-09-22 20:42:09 -0700

    space

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-for tens_digit in range(0,10):
+for tens_digit in range(0, 10):
     for ones_digit in range(tens_digit + 1, 10):
         if tens_digit != ones_digit:
             if tens_digit == 0 and ones_digit == 1:
