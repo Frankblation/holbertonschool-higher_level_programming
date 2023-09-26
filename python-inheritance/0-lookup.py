01ccbf2a04969631daa7e472477b4d3d153f32d1
Frankblation <frankblation@gmail.com>
2023-09-25 19:37:25 -0700

    ding dong

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+"""
+    look up and object
+"""
+
+
 def lookup(obj):
+    """return obj using dir func
+
+    Args:
+        obj (obj): object
+
+    Returns:
+        obj: object from dir
+    """
     return dir(obj)
