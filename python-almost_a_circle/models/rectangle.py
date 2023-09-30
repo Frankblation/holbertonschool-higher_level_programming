17b1f1686018c3738dc270b52c417978d2428f3b
Frankblation <frankblation@gmail.com>
2023-09-29 18:52:12 -0700

    okay lets see

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -16,10 +16,10 @@ class Rectangle(Base):
             id (int, optional): The ID of the rectangle. Defaults to None.
         """
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
