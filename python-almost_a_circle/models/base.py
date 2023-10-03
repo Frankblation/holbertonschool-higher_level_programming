e9930bd1fc6e7ca5cbb8f314ab3ecf5005f7d89d
Frankblation <frankblation@gmail.com>
2023-10-02 17:35:59 -0700

    [A

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -41,11 +41,3 @@ class Base:
         list_objs.append(object.to_dictionary())
         with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as file:
             file.write(cls.to_json_string(list_objs))
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        '''Saves object to file.'''
-        if list_objs is not None:
-            list_objs = [o.to_dictionary() for o in list_objs]
-        with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
-            f.write(cls.to_json_string(list_objs))
