7df8769f17269431cf52ea3ce0e3a28c85c4df77
Frankblation <frankblation@gmail.com>
2023-10-04 21:44:12 -0700

    flake

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,1 +1,45 @@
 #!/usr/bin/python3
+"""Rectangele that inherates base g"""
+
+
+class BaseGeometry:
+    """class basegeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be > 0")
+
+class Rectangle(BaseGeometry):
+    """inharets baseGeometry
+
+    Args:
+        BaseGeometry
+    """
+    def __init__(self, width, height):
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def __repr__(self):
+        return self.__str__()
+
+class Square(Rectangle):
+    """supersquare rectaangle"""
+    def __init__(self, size):
+        self.__size = self.integer_validator("size", size)
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
+
+    def __repr__(self):
+        return self.__str__()
