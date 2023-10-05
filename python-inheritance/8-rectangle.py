5cc9a6086ef6344576332fd3c032b61fc9e29372
Frankblation <frankblation@gmail.com>
2023-10-04 21:36:32 -0700

    now

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 """Rectangele that inherates base g"""
 
+class BaseGeometry:
+    """class basegeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be > 0")
 
 class Rectangle(BaseGeometry):
-    """inherates class base g"""
+    """inharets baseGeometry
+
+    Args:
+        BaseGeometry
+    """
     def __init__(self, width, height):
         self.__width = self.integer_validator("width", width)
         self.__height = self.integer_validator("height", height)
