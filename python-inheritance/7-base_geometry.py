6e27ff0c821c894aa30c6f0941ffbad6c7988a0e
Frankblation <frankblation@gmail.com>
2023-10-04 21:28:15 -0700

    7 base g

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""class basegeometry"""
+
+
+class BaseGeometry:
+    """calss basegeometry
+    def area as self,
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be > 0")
