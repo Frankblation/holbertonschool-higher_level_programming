856a676e8eb8c78cadb02add8b7ad9b8a84ad05d
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-10-20 10:47:18 -0500

    lets go

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- Create a table called 'first_table' to store unique identifiers and names.
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS 'first_table' (id INT, name VARCHAR(256));
