b1be7d3a280e57716dd2b0992cdd7b0fa8bc504f
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-10-20 10:56:32 -0500

    hbtn

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,2 +1,4 @@
--- Delete the database hbtn_0c_0 from the MySQL server,
+-- Delete the database hbtn_0c_0 from the MySQL server.
+-- If it doesn't exist do nothing.
+
 DROP DATABASE IF EXISTS 'hbtn_0c_0';
