577aff0a55b65755d4721e61bbfe70f5636de8f4
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-10-20 11:01:04 -0500

    2 comments no quotes

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,4 +1,4 @@
--- Delete the database hbtn_0c_0 from the MySQL server.
--- If it doesn't exist do nothing.
+-- Delete the database 'hbtn_0c_0' from the MySQL server.
+-- If 'hbtn_0c_0' doesn't exist script shouldn't fail.
 
-DROP DATABASE IF EXISTS 'hbtn_0c_0';
+DROP DATABASE IF EXISTS hbtn_0c_0;
