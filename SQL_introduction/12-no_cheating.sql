f232c582f0bd684e7548625bb7b43c2db6eb693d
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-10-20 22:49:19 -0500

    update

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,5 @@
 -- A script that updates the score
 -- of Bob to 10
--- in the table second_table.
+-- in the table second_table MySQL server.
 
 UPDATE second_table SET 'score' TO 10 WHERE 'name' = 'Bob'
