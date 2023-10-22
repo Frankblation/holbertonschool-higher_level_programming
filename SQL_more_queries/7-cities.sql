1461c0689ca0c97aaa2d5e8780b4608ed7378264
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-10-22 13:36:10 -0500

    it's an INT

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,7 +5,7 @@ USE `hbtn_0d_usa`;
 
 CREATE TABLE IF NOT EXISTS `cities` (
     id INT NOT NULL AUTO_INCREMENT,
-    state_id NOT NULL,
+    state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id),
     UNIQUE (id),
