036a9b8b29287e8a791c1fa6d36ad058234f42f8
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-10-22 11:27:03 -0500

    user grants

--- SQL_more_queries/0-privlages.sql
+++ SQL_more_queries/0-privlages.sql
@@ -1,5 +1,3 @@
--- Show privileges for user_0d_1
+-- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Show privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
