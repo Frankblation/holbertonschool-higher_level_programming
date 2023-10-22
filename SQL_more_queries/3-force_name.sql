a0de0444f1de42e8d8f83e0c1e48f8ba06a1ae42
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-10-22 12:15:23 -0500

    OKAY NO

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,5 @@
 -- SQL Script to create the force_name table in the specified database
-DROP TABLE IF EXISTS force_name;
 CREATE TABLE force_name (
-    id INT PRIMARY KEY DEFAULT 1,
+    id INT VALUE(1)
     name VARCHAR(256) NOT NULL
 );
