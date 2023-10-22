ead0624f5035c5bc1758a63dcb4e4f131a6fad00
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-10-22 12:03:09 -0500

    lets try this

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- SQL Script to create the force_name table in the specified database
 DROP TABLE IF EXISTS force_name;
 CREATE TABLE force_name (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY DEFAULT 1,
     name VARCHAR(256) NOT NULL
 );
