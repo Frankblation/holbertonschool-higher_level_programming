996db1249b1ca382e4da887ae595da0f90ba827b
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-10-23 02:10:45 -0500

    on?

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Script to join the cities and states tables
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states in states.id = cities.states_id
+INNER JOIN states ON states.id = cities.states_id
 ORDER cities.id ASC;
