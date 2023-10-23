bab6119488dfcf26dc9d7e5c23c8e5dbb2d2663c
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-10-23 01:27:46 -0500

    inner join redo

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Script to join the cities and states tables
-SELECT cities.id cities.name states.name
+SELECT cities.id, cities.name, states.name
 FROM cities
 INNER JOIN states in cities.id and states.id
-ORDER cities.id
+ORDER cities.id;
