520a5ffc7afa540128f56621fced675e36921601
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-11-22 16:19:07 -0600

    add 9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,1 +1,17 @@
 #!/opt/homebrew/bin/node
+
+function add (a, b) {
+  return a + b;
+}
+
+const [arg1 = NaN, arg2 = NaN] = process.argv.slice(2);
+
+const num1 = parseInt(arg1);
+const num2 = parseInt(arg2);
+
+if (!isNaN(num1) && !isNaN(num2)) {
+  const result = add(num1, num2);
+  console.log(result);
+} else {
+  console.log('NaN');
+}
