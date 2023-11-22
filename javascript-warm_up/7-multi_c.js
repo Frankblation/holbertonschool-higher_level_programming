89dfb4f90cdda12c6563938c56353c087dc6746b
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-11-22 15:46:28 -0600

    7 semi colon stoping loop

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,10 +3,10 @@ const [arg1 = undefined] = process.argv.slice(2);
 
 const x = parseInt(arg1);
 
-if (!isNaN(x)) {
-    for (let i = 0; i < x; i++); {
-        console.log("C is fun");
-    }
+if (!isNaN(x) && x > 0) {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 } else {
-    console.log("Missing number of occurrences")
+  console.log('Missing number of occurrences');
 }
