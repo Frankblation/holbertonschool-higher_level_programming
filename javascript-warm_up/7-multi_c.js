93466460819d0eb25f07c23c9c1b1a2277a70e7b
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-11-22 15:37:28 -0600

    7 multi c

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,1 +1,12 @@
-#!/opt/homebrew/bin/node
+#!/usr/bin/node
+const [arg1 = undefined] = process.argv.slice(2);
+
+const x = parseInt(arg1);
+
+if (!isNaN(x)) {
+    for (let i = 0; i < x; i++); {
+        console.log("C is fun");
+    }
+} else {
+    console.log("Missing number of occurrences")
+}
