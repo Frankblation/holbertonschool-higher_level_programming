7a77a6e06f0851b6bdcdd9254e7f6244e5915cc9
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-11-22 14:54:16 -0600

    5/ to int

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,1 +1,11 @@
-#!/opt/homebrew/bin/node
+#!/usr/bin/node
+
+const [arg1 = undefined] = process.argv.slice(2);
+
+const num = parseInt(arg1);
+
+if (!isNaN(num)) {
+  console.log(`My number: ${num}`);
+} else {
+  console.log('Not a number');
+}
