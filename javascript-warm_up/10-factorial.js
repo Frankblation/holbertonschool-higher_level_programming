69a90e6c63a3653b24fb1890c6ee80dad0e4ffc0
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-11-22 16:30:52 -0600

    10 is fact

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,1 +1,16 @@
-#!/opt/homebrew/bin/node
+#!/usr/bin/node
+const computeFactorial = (n) => {
+  if (isNaN(n) || !Number.isInteger(Number(n))) {
+    return 1;
+  } else if (n < 0) {
+    return Infinity;
+  } else if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * computeFactorial(n - 1);
+  }
+};
+
+const inputNumber = process.argv[2];
+const result = computeFactorial(inputNumber);
+console.log(result);
