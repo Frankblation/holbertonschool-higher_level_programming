b1cd5dd112ae0dcbd3181d6d397cee0e53668073
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-11-22 16:34:21 -0600

    function added

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,16 +1,19 @@
-#!/usr/bin/node
-const computeFactorial = (n) => {
-  if (isNaN(n) || !Number.isInteger(Number(n))) {
+#!/opt/homebrew/bin/node
+
+function factorial (n) {
+  if (isNaN(n) || n < 0) {
     return 1;
-  } else if (n < 0) {
-    return Infinity;
-  } else if (n === 0 || n === 1) {
+  } else if (n === 0) {
     return 1;
   } else {
-    return n * computeFactorial(n - 1);
+    return n * factorial(n - 1);
   }
-};
+}
 
-const inputNumber = process.argv[2];
-const result = computeFactorial(inputNumber);
+const [arg = NaN] = process.argv.slice(2);
+
+const num = parseInt(arg);
+
+const result = factorial(num);
+
 console.log(result);
