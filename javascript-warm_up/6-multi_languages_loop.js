74c122af66abc26cf326a939f6968f79b4b01e5e
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-11-22 15:25:47 -0600

    6-multi

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,6 @@
-#!/opt/homebrew/bin/node
+#!/usr/bin/node
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
