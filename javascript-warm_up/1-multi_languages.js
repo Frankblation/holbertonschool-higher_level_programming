8ddc366c2895f5efcb87ec0a6039ddc7ee262d8a
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-11-22 13:21:24 -0600

    print all 3

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const myVar1 = 'C is fun';
+console.log(myVar1);
+
+let myVar2 = 'Python is cool';
+console.log(myVar2);
+
+myVar2 = 'JavaScript is amazing';
+console.log(myVar2);
