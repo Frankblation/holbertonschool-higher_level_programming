e5bff0f79bf5c362fa577cdaa796702e5b25104e
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-11-23 02:04:13 -0600

    12

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,1 +1,13 @@
-#!/opt/homebrew/bin/node
+#!/usr/bin/node
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+
+console.log(myObject);
+
+// Replace the value 12 with 89
+myObject.value = 89;
+
+console.log(myObject);
