eb5a1e18d4ecb056adb2bdf5d524fc22d016fdc3
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-11-23 01:48:36 -0600

    11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,11 @@
-#!/opt/homebrew/bin/node
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+const numbers = args.map(arg => parseInt(arg));
+
+const validNumbers = numbers.filter(num => !isNaN(num));
+
+const sortedNumbers = validNumbers.sort((a, b) => b - a);
+
+console.log(sortedNumbers.length >= 2 ? sortedNumbers[1] : 0);
