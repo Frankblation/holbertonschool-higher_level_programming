2b0a45863652b313b0c90b2f5d3dd1542c2ad5be
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-11-28 16:38:51 -0600

    git pulled finally

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
-
 class Rectangle {
     constructor(w, h) {
-        if (!Number.isInteger(w) || !Number.isInteger(h) || w <= 0 || h <= 0) {
-            // If w or h is not a positive integer, create an empty object
-            this.width = 0;
-            this.height = 0;
-        } else {
-            // Initialize the instance attributes width and height
-            this.width = w;
-            this.height = h;
-        }
+      if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+        return {};
+      } else {
+        this.width = w;
+        this.height = h;
+      }
     }
-}
-module.exports = Rectangle;
+  }
