c77de2d63389269af508826286f5edc51e070893
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-11-28 16:58:15 -0600

    change

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 class Rectangle {
     constructor(w, h) {
-      if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-        return {};
-      } else {
-        this.width = w;
-        this.height = h;
+      if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+        // Throw an error if conditions are not met
+        throw new Error("Invalid arguments. Width and height must be positive numbers.");
       }
+
+      // Initialize instance attributes
+      this.width = w;
+      this.height = h;
     }
   }
