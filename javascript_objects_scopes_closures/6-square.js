c53aa1d030c425f508fa71e4c254c1ca0f4c7e29
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-11-28 18:55:37 -0600

    commit

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const SquareBase = require('./5-square');
+
+class Square extends SquareBase {
+  constructor (size) {
+    super(size);
+  }
+
+  charPrint (c) {
+    const character = c || 'X';
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(character.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
