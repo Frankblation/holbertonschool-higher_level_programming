5d0b69a6673f2ed2e0008c911cb756741326be11
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-12-04 17:41:36 -0600

    try 2 again added files

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-if (process.argv.length !== 3) {
-  console.error('Usage: node 2-statuscode.js <url>');
-  process.exit(1);
-}
-
 const url = process.argv[2];
 
 request.get(url, (error, response) => {
